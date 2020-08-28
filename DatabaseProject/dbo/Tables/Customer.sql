9fe834d4e636b47a801b819902ddd9792ff52db0
mahesh reddy <49952499+13k81a0229@users.noreply.github.com>
2020-08-28 22:03:09 +0530

    Add files via upload
    Main Solution file folder

---
+++ DatabaseProject/dbo/Tables/Customer.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Customer]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NULL, 
+    [Email] NVARCHAR(50) NULL , 
+    [Phone] NUMERIC NULL, 
+    [Address] NVARCHAR(MAX) NULL, 
+    CONSTRAINT [FK_Customer_ToTable] FOREIGN KEY (Email) REFERENCES [employee]([email])
+)
---
+++ DatabaseProject/dbo/Tables/ProductTable.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ProductTable]
+(
+	[ProductId] INT NOT NULL PRIMARY KEY, 
+    [ProductName] NVARCHAR(50) NULL, 
+    [saleID] INT NULL, 
+    [SaleDate] DATE NULL, 
+    [SaleLocation] NVARCHAR(50) NULL
+)
